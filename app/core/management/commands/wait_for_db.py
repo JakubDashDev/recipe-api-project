0a61b2a1d88503db872c72549a3dbce9a29009ad
Jakub Cieślik <kuba8172@gmail.com>
2025-02-24 21:17:53 +0100

    Remove whitespaces

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -27,5 +27,5 @@ class Command(BaseCommand):
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavaiable, waiting 1 second...')
                 time.sleep(1)
-                
+ 
         self.stdout.write(self.style.SUCCESS('Database available!'))
