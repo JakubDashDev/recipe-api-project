810b5f5c3bf09626508720ff3eca359331a32b4b
Jakub Cieślik <kuba8172@gmail.com>
2025-03-12 13:30:57 +0100

    Tag URL

---
+++ app/tag/urls.py
@@ -0,0 +1,14 @@
+"""URL mapping for the reicpe API"""
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from tag import views
+
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+
+app_name = 'tag'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
